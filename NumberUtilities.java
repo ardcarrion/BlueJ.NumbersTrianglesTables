fab1f1ec905646050c018d8725a1824c606c894c
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-05-23 09:57:24 -0400

    without using Math.pow

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,71 @@
 
-
-
 public class NumberUtilities {
-
+    
     public static String getRange(int stop) {
-        return null;
+        String outcome = "";
+
+        for (int i = 0; i < stop; i++) {
+            outcome = outcome + String.valueOf(i); 
+        }
+        System.out.println(outcome);
+        return outcome;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String outcome = "";
+
+        for (int i = start; i < stop; i++) {
+            outcome = outcome + String.valueOf(i);
+        }
+        System.out.println(outcome);
+        return outcome;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String outcome = "";
+        for (int i = start; i < stop; i += step) {
+            outcome = outcome + String.valueOf(i);
+        }
+        System.out.println(outcome);
+        return outcome;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String outcome = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) outcome = outcome + String.valueOf(i);
+        }
+        System.out.println(outcome);
+        return outcome;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String outcome = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 1) outcome = outcome + String.valueOf(i);
+        }
+        System.out.println(outcome);
+        return outcome;
     }
 
-
+    private static int exponentiation(int base, int exponent) {
+        int result = base;
+        for (int i = 1; i < exponent; i++) {
+            result = result*base;
+        }
+        return result;
+    }
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String outcome = "";
+
+        for (int i = start; i <= stop; i++) {
+            int result = exponentiation(i, exponent);
+            outcome = outcome + String.valueOf(result);
+        }
+        System.out.println(outcome);
+        return outcome;
+        
     }
 }
