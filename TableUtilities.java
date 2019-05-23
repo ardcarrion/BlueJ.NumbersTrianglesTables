7543c19ea554fae04cc8987debf4d9086c0b0a6d
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-05-23 10:11:34 -0400

    finished

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,22 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String outcome = "";
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                String cell = String.format("%3d |", i*j);
+                outcome += cell;
+            }
+            outcome += "\n";
+        }
+        return outcome;
     }
 }
